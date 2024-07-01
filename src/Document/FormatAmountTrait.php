69fa5630edb5068ebcb3f2b3ef147580625c5c37
Joachim Løvgaard <joachim@loevgaard.dk>
2024-07-01 14:39:13 +0200

    Add todo

--- src/Document/FormatAmountTrait.php
+++ src/Document/FormatAmountTrait.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Setono\SyliusMeilisearchPlugin\Document;
 
+// todo include a functions file instead
 trait FormatAmountTrait
 {
     protected static function formatAmount(int $amount): float
