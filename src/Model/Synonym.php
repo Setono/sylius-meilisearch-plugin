3af518dc4565d1ddb671776208503dbfe83b7f01
Joachim Løvgaard <joachim@loevgaard.dk>
2024-08-21 21:53:55 +0200

    Improve synonym functionality

--- src/Model/Synonym.php
+++ src/Model/Synonym.php
@@ -4,12 +4,19 @@ declare(strict_types=1);
 
 namespace Setono\SyliusMeilisearchPlugin\Model;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Sylius\Component\Channel\Model\ChannelInterface;
 use Sylius\Component\Locale\Model\LocaleInterface;
+use Sylius\Component\Resource\Model\TimestampableTrait;
+use Sylius\Component\Resource\Model\ToggleableTrait;
 use function Symfony\Component\String\u;
 
 class Synonym implements SynonymInterface
 {
+    use TimestampableTrait;
+    use ToggleableTrait;
+
     protected ?int $id = null;
 
     protected ?string $term = null;
@@ -18,7 +25,16 @@ class Synonym implements SynonymInterface
 
     protected ?LocaleInterface $locale = null;
 
-    protected ?ChannelInterface $channel = null;
+    /** @var Collection<array-key, ChannelInterface> */
+    protected Collection $channels;
+
+    /** @var list<string>|null */
+    protected ?array $indexes = null;
+
+    public function __construct()
+    {
+        $this->channels = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -55,13 +71,53 @@ class Synonym implements SynonymInterface
         $this->locale = $locale;
     }
 
-    public function getChannel(): ?ChannelInterface
+    public function getChannels(): Collection
     {
-        return $this->channel;
+        return $this->channels;
     }
 
-    public function setChannel(?ChannelInterface $channel): void
+    public function addChannel(ChannelInterface $channel): void
     {
-        $this->channel = $channel;
+        if (!$this->hasChannel($channel)) {
+            $this->channels->add($channel);
+        }
+    }
+
+    public function removeChannel(ChannelInterface $channel): void
+    {
+        if ($this->hasChannel($channel)) {
+            $this->channels->removeElement($channel);
+        }
+    }
+
+    public function hasChannel(ChannelInterface $channel): bool
+    {
+        return $this->channels->contains($channel);
+    }
+
+    public function getIndexes(): array
+    {
+        return $this->indexes ?? [];
+    }
+
+    public function addIndex(string $index): void
+    {
+        $this->indexes[] = $index;
+    }
+
+    public function removeIndex(string $index): void
+    {
+        $indexes = $this->getIndexes();
+        $key = array_search($index, $indexes, true);
+        if ($key !== false) {
+            unset($indexes[$key]);
+        }
+
+        $this->indexes = [] === $indexes ? null : array_values($indexes);
+    }
+
+    public function hasIndex(string $index): bool
+    {
+        return in_array($index, $this->getIndexes(), true);
     }
 }
