46a231ac201bc05aa671ebeaf7fbc602846c4323
Joachim Løvgaard <joachim@loevgaard.dk>
2024-08-26 12:06:41 +0200

    Make the TaxonIndexScopeProvider more correct: Only use locales enabled on channels

--- src/Provider/IndexScope/TaxonIndexScopeProvider.php
+++ src/Provider/IndexScope/TaxonIndexScopeProvider.php
@@ -5,32 +5,46 @@ declare(strict_types=1);
 namespace Setono\SyliusMeilisearchPlugin\Provider\IndexScope;
 
 use Setono\SyliusMeilisearchPlugin\Config\Index;
+use Sylius\Component\Channel\Context\ChannelContextInterface;
+use Sylius\Component\Channel\Repository\ChannelRepositoryInterface;
+use Sylius\Component\Core\Model\ChannelInterface;
 use Sylius\Component\Core\Model\TaxonInterface;
 use Sylius\Component\Locale\Context\LocaleContextInterface;
-use Sylius\Component\Locale\Model\LocaleInterface;
-use Sylius\Component\Resource\Repository\RepositoryInterface;
 
 final class TaxonIndexScopeProvider implements IndexScopeProviderInterface
 {
     public function __construct(
-        private readonly RepositoryInterface $localeRepository,
+        private readonly ChannelRepositoryInterface $channelRepository,
+        private readonly ChannelContextInterface $channelContext,
         private readonly LocaleContextInterface $localeContext,
     ) {
     }
 
     public function getAll(Index $index): iterable
     {
-        /** @var LocaleInterface[] $locales */
-        $locales = $this->localeRepository->findAll();
+        /** @var ChannelInterface[] $channels */
+        $channels = $this->channelRepository->findBy([
+            'enabled' => true,
+        ]);
 
-        foreach ($locales as $locale) {
-            yield new IndexScope(index: $index, localeCode: $locale->getCode());
+        foreach ($channels as $channel) {
+            foreach ($channel->getLocales() as $locale) {
+                yield new IndexScope(
+                    index: $index,
+                    channelCode: $channel->getCode(),
+                    localeCode: $locale->getCode(),
+                );
+            }
         }
     }
 
     public function getFromContext(Index $index): IndexScope
     {
-        return new IndexScope(index: $index, localeCode: $this->localeContext->getLocaleCode());
+        return new IndexScope(
+            index: $index,
+            channelCode: $this->channelContext->getChannel()->getCode(),
+            localeCode: $this->localeContext->getLocaleCode(),
+        );
     }
 
     public function supports(Index $index): bool
