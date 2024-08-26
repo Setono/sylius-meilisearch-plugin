555582fc014a4bc014befd9ce6d55b42732f9fae
Joachim Løvgaard <joachim@loevgaard.dk>
2024-08-26 20:13:02 +0200

    Remove obvious comment because @Zales0123 doesn't like obvious comments :joy:

--- src/Document/UrlAwareInterface.php
+++ src/Document/UrlAwareInterface.php
@@ -6,8 +6,5 @@ namespace Setono\SyliusMeilisearchPlugin\Document;
 
 interface UrlAwareInterface
 {
-    /**
-     * Sets the URL on the respective document
-     */
     public function setUrl(string $url): void;
 }
