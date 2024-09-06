14e8080a0bd3d4716184998de29c0366893b78b4
Mateusz Zalewski <zaleslaw@gmail.com>
2024-09-06 11:22:57 +0200

    Use sorting library

--- src/Form/Builder/Sorter/SizeSorter.php
+++ src/Form/Builder/Sorter/SizeSorter.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace Setono\SyliusMeilisearchPlugin\Form\Builder\Sorter;
 
+use DragonCode\SizeSorter\Sorter;
+
 final class SizeSorter implements ChoiceValuesSorterInterface
 {
     public function sort(array $choices): array
     {
-        return array_merge(array_flip(array_values(array_intersect(['S', 'M', 'L', 'XL', 'XXL'], $choices))), $choices);
+        return Sorter::sort($choices)->toArray();
     }
 }
