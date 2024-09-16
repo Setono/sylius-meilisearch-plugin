c8da6be8eb26b9e5fe98260a73c8feb99db9d761
Mateusz Zalewski <zaleslaw@gmail.com>
2024-09-16 17:23:53 +0200

    Add some more search tests

--- tests/Functional/SearchSortingTest.php
+++ tests/Functional/SearchSortingTest.php
@@ -4,7 +4,15 @@ declare(strict_types=1);
 
 namespace Setono\SyliusMeilisearchPlugin\Tests\Functional;
 
+use function Amp\Promise\wait;
+use Doctrine\ORM\EntityManagerInterface;
 use Setono\SyliusMeilisearchPlugin\Engine\SearchEngine;
+use Setono\SyliusMeilisearchPlugin\Message\Command\Index;
+use Sylius\Component\Core\Model\ChannelPricingInterface;
+use Sylius\Component\Core\Model\ProductInterface;
+use Sylius\Component\Core\Model\ProductVariantInterface;
+use Sylius\Component\Core\Repository\ProductRepositoryInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 /** @group functional */
 final class SearchSortingTest extends FunctionalTestCase
@@ -51,5 +59,67 @@ final class SearchSortingTest extends FunctionalTestCase
             self::assertLessThanOrEqual($previousHit['createdAt'], $hit['createdAt']);
             $previousKey = $key;
         }
+    }
+
+    public function testItSortsResultsByBiggestDiscount(): void
+    {
+        $this->changeProductPrice('990M_regular_fit_jeans', 10);
+
+        /** @var SearchEngine $searchEngine */
+        $searchEngine = self::getContainer()->get(SearchEngine::class);
+        $result = $searchEngine->execute('jeans', ['sort' => 'discount:desc']);
+
+        /** @var array $firstHit */
+        $firstHit = $result->getHit(0);
+        self::assertSame(0.1, $firstHit['price']);
+        self::assertGreaterThan(0, $firstHit['discount']);
+
+        $this->resetProductPrices();
+    }
+
+    private function changeProductPrice(string $productCode, int $price): void
+    {
+        $channelPricing = $this->getChannelPricingOfProduct($productCode);
+        $channelPricing->setOriginalPrice($channelPricing->getPrice());
+        $channelPricing->setPrice($price);
+
+        $this->saveAndReindexProduct();
+    }
+
+    private function resetProductPrices(): void
+    {
+        $channelPricing = $this->getChannelPricingOfProduct('990M_regular_fit_jeans');
+        $channelPricing->setPrice($channelPricing->getOriginalPrice());
+        $channelPricing->setOriginalPrice(null);
+
+        $this->saveAndReindexProduct();
+    }
+
+    private function getChannelPricingOfProduct(string $code): ChannelPricingInterface
+    {
+        /** @var ProductRepositoryInterface $productRepository */
+        $productRepository = self::getContainer()->get('sylius.repository.product');
+        /** @var ProductInterface $product */
+        $product = $productRepository->findOneBy(['code' => $code]);
+        /** @var ProductVariantInterface $variant */
+        $variant = $product->getVariants()->first();
+        /** @var ChannelPricingInterface $channelPricing */
+        $channelPricing = $variant->getChannelPricings()->first();
+
+        return $channelPricing;
+    }
+
+    private function saveAndReindexProduct(): void
+    {
+        /** @var EntityManagerInterface $productManager */
+        $productManager = self::getContainer()->get('sylius.manager.product');
+        $productManager->flush();
+
+        /** @var MessageBusInterface $commandBus */
+        $commandBus = self::getContainer()->get('sylius.command_bus');
+        $commandBus->dispatch(new Index('products'));
+
+        // we need to wait for reindexing, at least a little bit :/
+        sleep(1);
     }
 }
--- tests/Functional/SearchTest.php
+++ tests/Functional/SearchTest.php
@@ -18,6 +18,28 @@ final class SearchTest extends FunctionalTestCase
         self::assertSame(8, $result->getHitsCount());
     }
 
+    public function testItProvidesSearchResultByMultipleCriteria(): void
+    {
+        /** @var SearchEngine $searchEngine */
+        $searchEngine = self::getContainer()->get(SearchEngine::class);
+        $result = $searchEngine->execute(
+            'jeans',
+            [
+                'facets' => [
+                    'brand' => ['Celsius small', 'You are breathtaking'],
+                    'price' => ['min' => '30', 'max' => '45'],
+                ],
+            ],
+        );
+
+        /** @var array $hit */
+        $hit = $result->getHit(0);
+
+        self::assertLessThan(45, (int) $hit['price']);
+        self::assertGreaterThan(30, (int) $hit['price']);
+        self::assertTrue(in_array(((array) $hit['brand'])[0], ['Celsius small', 'You are breathtaking'], true));
+    }
+
     public function testItAlwaysDisplaysFullFacetDistribution(): void
     {
         /** @var SearchEngine $searchEngine */
