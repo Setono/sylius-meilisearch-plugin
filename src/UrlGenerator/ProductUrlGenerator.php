3f2dda7932ce87c2c1f0edca8a68295b6e2259fd
Victor Vasiloi <victor.vasiloi@gmail.com>
2025-01-22 15:24:05 +0100

    Only pass the _locale param when it's required

--- src/UrlGenerator/ProductUrlGenerator.php
+++ src/UrlGenerator/ProductUrlGenerator.php
@@ -6,18 +6,32 @@ namespace Setono\SyliusMeilisearchPlugin\UrlGenerator;
 
 use Setono\SyliusMeilisearchPlugin\Model\IndexableInterface;
 use Sylius\Component\Core\Model\ProductInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Routing\RouterInterface;
 use Webmozart\Assert\Assert;
 
 final class ProductUrlGenerator extends AbstractEntityUrlGenerator
 {
+    public function __construct(private RouterInterface $router)
+    {
+        parent::__construct($router);
+    }
+
     public function generate(IndexableInterface $entity, array $context = []): string
     {
         Assert::true($this->supports($entity, $context));
 
-        return $this->urlGenerator->generate('sylius_shop_product_show', [
+        $params = [
             'slug' => $entity->getTranslation($context['localeCode'])->getSlug(),
-            '_locale' => $context['localeCode'],
-        ]);
+        ];
+
+        $route = $this->router->getRouteCollection()->get('sylius_shop_product_show');
+
+        if ($route !== null && $route->hasRequirement('_locale')) {
+            $params['_locale'] = $context['localeCode'];
+        }
+
+        return $this->router->generate('sylius_shop_product_show', $params);
     }
 
     /**
