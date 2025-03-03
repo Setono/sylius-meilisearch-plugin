dcbfc11923bbc3ca5e055a1f260e50d15adc65a6
Sébastien <sebastien@emagma.fr>
2025-03-03 18:10:27 +0100

    Fix indexable deletion in EntityListener

--- src/EventListener/Doctrine/EntityListener.php
+++ src/EventListener/Doctrine/EntityListener.php
@@ -8,13 +8,18 @@ use Doctrine\Persistence\Event\LifecycleEventArgs;
 use Setono\SyliusMeilisearchPlugin\Message\Command\IndexEntity;
 use Setono\SyliusMeilisearchPlugin\Message\Command\RemoveEntity;
 use Setono\SyliusMeilisearchPlugin\Model\IndexableInterface;
+use SplObjectStorage;
 use Sylius\Component\Resource\Model\TranslationInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
 
 final class EntityListener
 {
-    public function __construct(private readonly MessageBusInterface $commandBus)
-    {
+    private readonly SplObjectStorage $removeIndexableStorage;
+
+    public function __construct(
+        private readonly MessageBusInterface $commandBus,
+    ) {
+        $this->removeIndexableStorage = new SplObjectStorage();
     }
 
     public function postPersist(LifecycleEventArgs $eventArgs): void
@@ -27,9 +32,34 @@ final class EntityListener
         $this->dispatch($eventArgs, static fn (IndexableInterface $entity) => IndexEntity::new($entity));
     }
 
+    public function preRemove(LifecycleEventArgs $eventArgs): void
+    {
+        $indexable = self::extractIndexableFromEvent($eventArgs);
+
+        if (null !== $indexable) {
+            $this->removeIndexableStorage->attach($indexable, $indexable->getId());
+        }
+    }
+
     public function postRemove(LifecycleEventArgs $eventArgs): void
     {
-        $this->dispatch($eventArgs, static fn (IndexableInterface $entity) => RemoveEntity::new($entity));
+        $indexable = self::extractIndexableFromEvent($eventArgs);
+
+        if (null === $indexable) {
+            return;
+        }
+
+        /** @var ?int $entityId */
+        $entityId = $this->removeIndexableStorage[$indexable] ?? null;
+
+        if (null === $entityId) {
+            return;
+        }
+
+        $this->dispatch(
+            $eventArgs,
+            fn (IndexableInterface $entity) => new RemoveEntity($entity::class, $entityId),
+        );
     }
 
     /**
@@ -37,16 +67,25 @@ final class EntityListener
      */
     private function dispatch(LifecycleEventArgs $eventArgs, callable $message): void
     {
+        $indexable = self::extractIndexableFromEvent($eventArgs);
+
+        if (null !== $indexable) {
+            $this->commandBus->dispatch($message($indexable));
+        }
+    }
+
+    private static function extractIndexableFromEvent(LifecycleEventArgs $eventArgs): ?IndexableInterface
+    {
         $obj = $eventArgs->getObject();
 
         if ($obj instanceof TranslationInterface) {
             $obj = $obj->getTranslatable();
         }
-        
+
         if (!$obj instanceof IndexableInterface) {
-            return;
+            return null;
         }
 
-        $this->commandBus->dispatch($message($obj));
+        return $obj;
     }
 }
